eef9ccbe43e7206ab34e54958526e834a4b2f6c5
Jaysins <30625480+Jaysins@users.noreply.github.com>
2024-07-17 01:41:06 +0100

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,6 +1,6 @@
 variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
-  default     = "my-unique-bucket-name-12345"
+  default     = "438-execution-bucket"
 }
 
