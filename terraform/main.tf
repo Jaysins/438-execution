43f5122f65e70f84a22fb50714ba9a351c69fdd8
Jaysins <30625480+Jaysins@users.noreply.github.com>
2024-07-17 01:43:58 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_s3_bucket" "438_bucket" {
-  bucket = "438-execution-bucket"
+resource "aws_s3_bucket" "execution_test" {
+  bucket = "execution_test_bucket"
 }
 
-resource "aws_s3_bucket_acl" "438_execution_acl" {
-  bucket = aws_s3_bucket.438_bucket.bucket
+resource "aws_s3_bucket_acl" "execution_test_acl" {
+  bucket = aws_s3_bucket.execution_test.bucket
   acl    = "private"
 }
